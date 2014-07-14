99bebd2ce8da65ca0f708cfafe0cab97d4af0f83
Rajiv Thamburaj <rajivthamburaj@gmail.com>
2014-07-13 23:22:33 -0400

    Add more detailed comments

--- PyTracer/display.py
+++ PyTracer/display.py
@@ -31,6 +31,7 @@ class World(object):
 		"""
 		Places objects in the World
 		"""
+		# Set up the screen rectangle (stores information to render the scene)
 		self.sr = ScreenRect()
 		self.sr.screen_width = 200
 		self.sr.screen_height = 200
@@ -38,21 +39,25 @@ class World(object):
 		self.sr.num_samples = 1
 		self.sr.sampler = sample.UniformSampler(self.sr.num_samples)
 		
+		# Set up instance variables
 		self.background_color = const.BLACK
 		self.tracer = MultiplePrimitivesTracer(self)
 		self.view_distance = 200
 		self.plane_distance = 100
 		
+		# Sphere 1
 		center = np.array([0, -25, 0], float)
 		radius = 80
 		color = const.RED
 		sphere_1 = solids.Sphere(center, radius, color)
 		
+		# Sphere 2
 		center = np.array([0, 30, 0], float)
 		radius = 60
 		color = const.YELLOW
 		sphere_2 = solids.Sphere(center, radius, color)
 		
+		# Plane
 		point = np.array([0, 0, 0], float)
 		normal = np.array([0, 1, 1], float)
 		color = np.array([0, 0.3, 0], float)
@@ -67,44 +72,49 @@ class World(object):
 		# Origin for the rays on the z-axis
 		z_w = 100.0
 		ray_direction = np.array([0, 0, -1], float)
+		# Find the side length of the sample rectangle
 		n = int(math.sqrt(self.sr.num_samples))
 		
 		for i in xrange(0, self.sr.screen_height):
 			self.print_progress(i)
 			for j in xrange(0, self.sr.screen_width):
 				pixel_color = const.BLACK
-				
 				n = self.sr.num_samples
+				# Loop through all samples
 				for p in xrange(n):
 					# Get the next sampling point in the set [0,1] x [0,1]
 					sample_point = self.sr.sampler.get_square_sample()
-					# Find the points within the current pixel to sample
+					# Find the point within the current pixel to sample
 					x = self.sr.pixel_width * (j - 0.5*self.sr.screen_width + sample_point[0])
 					y = self.sr.pixel_width * (i - 0.5*self.sr.screen_height + sample_point[1])
 					
 					ray_origin = np.array([x, y, z_w], float)
+					# Add the color to the current value
 					pixel_color = pixel_color + self.tracer.trace_ray(ray_origin, ray_direction)
 
-				# Take the average of each of the colors
+				# Take the average of the colors
 				pixel_color = pixel_color*(1.0/self.sr.num_samples)
+				# Store the pixel color
 				self.add_pixel(i, j, pixel_color)
 	
 	def render_perspective(self):
 		"""
 		Renders the image, pixel by pixel, with perspective tracing
 		"""
-		# Origin for the rays on the z-axis
+		# Origin for the rays on the z-axis (common)
 		ray_origin = np.array([0, 0, self.view_distance], float)
 		
 		for i in xrange(0, self.sr.screen_height):
 			self.print_progress(i)
 			for j in xrange(0, self.sr.screen_width):
+				# Find the direction of the ray
 				d_x = self.sr.pixel_width * (j - 0.5*(self.sr.screen_width - 1.0))
 				d_y = self.sr.pixel_width * (i - 0.5*(self.sr.screen_height - 1.0))
 				d_z = -self.plane_distance
 				ray_direction = np.array([d_x, d_y, d_z], float)
 				ray_direction = ray_direction * (1.0/np.linalg.norm(ray_direction))
 				
+				# Find the color of the pixel and store it
 				pixel_color = self.tracer.trace_ray(ray_origin, ray_direction)
 				self.add_pixel(i, j, pixel_color)
 	
@@ -142,8 +152,10 @@ class World(object):
 		t_min = float("inf")
 		shade_rectangle = ShadeRectangle(self)
 		
+		# Loop through all objects
 		for object in self.objects:
 			if object.did_hit(ray_origin, ray_direction, shade_rectangle):
+				# Find the closest point of intersection
 				if object.t_min < t_min:
 					shade_rectangle.did_hit = True
 					t_min = object.t_min
--- PyTracer/sample.py
+++ PyTracer/sample.py
@@ -21,20 +21,27 @@ class Sampler(object):
 		"""
 		self.num_samples = num_samples
 		self.sample_index = 0
+		# We have 83 sets because 83 is a relatively large (for our purposes) prime number
 		self.num_sets = 83
 		self.samples = []
 		
+		# Sample generation is passed on to the subclass
 		self.generate_samples()
 		self.shuffle_indices()
 	
 	def get_square_sample(self):
 		"""
-		Returns the next point to be sampled in the set [0,1] x [0,1]
+		Returns the next point to be sampled in the set [0,1] x [0,1]. The sample points are
+		generated ahead of time, so we must determine all the sets prior to ray-tracing and
+		access the samples sequentially. To avoid sampling errors, we add some randomness to the
+		process.
 		"""
 		# Sets are accessed in a random order to eliminate matching samples in rows/columns
 		if (self.sample_index % self.num_samples) == 0:
 			self.skip = (random.randrange(999) % self.num_sets) * self.num_samples
 		
+		# Within the current set, visit the current sample index (making sure not to exceed the
+		# bounds of the list)
 		index = self.skip + self.shuffled_indices[(self.skip + self.sample_index) % self.num_samples]
 		next_sample = self.samples[index]
 		self.sample_index += 1
@@ -62,7 +69,7 @@ class UniformSampler(Sampler):
 	
 	def generate_samples(self):
 		"""
-		Generate the samples on a unit square
+		Generate the samples on a unit square (optimal for 1 sample)
 		"""
 		n = int(math.sqrt(self.num_samples))
 				
